0893127130bef7fb2f4e41bad349b9155ed4fed9
proshantosaha <proshantoll71@gmail.com>
2024-01-13 12:39:20 +0600

    completed design and passing props tictoctoe

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import Square from "./bord/Square";
 
-const App = () => {
-  return <div>App</div>;
+const Game = () => {
+  return (
+    <>
+      <div>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      <div>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      <div>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </>
+  );
 };
-
-export default App;
+export default Game;
